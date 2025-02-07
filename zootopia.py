df5f84a568d8440bf0f15339decf8e339bc8f229
ateetvatan <ateetph@gmail.com>
2025-02-07 10:36:48 +0100

    Added Bonus step.

--- zootopia.py
+++ zootopia.py
@@ -1,5 +1,4 @@
 """THe Main Zootopia Module"""
-from typing import Type
 from data.schema_data_handler import SchemaDataHandler
 from data.animal_metadata_schema import AnimalMetadataSchemaRoot
 from web.animals_web_generator import AnimalWebGenerator
@@ -7,11 +6,18 @@ from web.animals_web_generator import AnimalWebGenerator
 
 def init_zootopia():
     """Main function for Zootopia application."""
-    sdh = SchemaDataHandler("animals_data.json", "data", AnimalMetadataSchemaRoot)  # The schema class instance.
-    data = sdh.data  # Json data from file cast to the AnimalMetadataSchemaRoot class
-    awg = AnimalWebGenerator("animals_template.html", "") # AnimalWebGenerator reads data from animals_template.html.
-    search_keys = ("name", "diet", "locations", "type")  # The Keys to be searched and display in HTML.
-    awg.display_key_info_in_html(data, search_keys, search_keys[0], "animals.html") # All searched keys are written to a file animals.html
+    # The schema class instance.
+    sdh = SchemaDataHandler("animals_data.json", "data", AnimalMetadataSchemaRoot)
+    # Json data from file cast to the AnimalMetadataSchemaRoot class
+    data = sdh.data
+    # AnimalWebGenerator reads data from animals_template.html.
+    awg = AnimalWebGenerator("animals_template.html", "")
+    # The Keys to be searched and display in HTML.
+    search_keys = ("name", "diet", "locations", "type", "kingdom", "slogan", "diet",
+                   "average_litter_size")
+    # All searched keys are written to a file animals.html
+    awg.display_key_info_in_html(data, search_keys, search_keys[0],
+                                 "animals.html")
 
 
 if __name__ == "__main__":
