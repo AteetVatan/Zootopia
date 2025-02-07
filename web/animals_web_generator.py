13c26a97906c2379516ae6c5f976cf463fe2c128
ateetvatan <ateetph@gmail.com>
2025-02-07 10:28:24 +0100

    improve card item html.

--- web/animals_web_generator.py
+++ web/animals_web_generator.py
@@ -11,11 +11,11 @@ class AnimalWebGenerator:
     def __init__(self, file_name, file_dir):
         self.__data_handler = HtmlDataHandler(file_name, file_dir)
 
-    def display_key_info_in_html(self, json_data, keys, file_name):
+    def display_key_info_in_html(self, json_data, keys, title_key, file_name):
         """Method to display the data keys result in HTML."""
         dq = DataQuery(json_data)
         results = dq.query_data(*keys)
-        key_html_list = AnimalWebGenerator.__get_key_results_html(results)
+        key_html_list = AnimalWebGenerator.__get_key_results_html(title_key, results)
         modified_html = (AnimalWebGenerator.
                          __modify_html_with_key_result(self.__data_handler.data, key_html_list))
         self.__data_handler.write_data(modified_html, "", file_name)
@@ -31,22 +31,30 @@ class AnimalWebGenerator:
         return html_data.replace(AnimalWebGenerator.HTML_PLACEHOLDER, key_html)
 
     @staticmethod
-    def __get_key_results_html(data_list: list[dict]) -> str:
+    def __get_key_results_html(title_key: str, data_list: list[dict]) -> str:
         """
         Function to print the keys in data
-        :param data: The pydantic data object.
-        :param keys: Searched keys tuple.
+        :param title_key: The Title Key header.
+        :param data_list: The animal keys data list.
         """
         line_list = []
         for item in data_list:
             line_list.append('<li class="cards__item">\n')
+            title = ""
+            sub_line_list = []
             for k, v in item.items():
                 if isinstance(v, list):
                     val = str(v[0])
                 else:
                     val = str(v)
-
-                line_list.append(f"{k.title()}: {val.title()}<br/>\n")
+                if k == title_key:
+                    title = f'<div class="card__title">{val.title()}</div>\n'
+                else:
+                    sub_line_list.append(f"<strong>{k.title()}:</strong> {val.title()}<br/>\n")
+            line_list.append(title)
+            line_list.append('<p class="card__text">')
+            line_list.extend(sub_line_list)
+            line_list.append('</p>')
             line_list.append('</li>\n')
 
         res = "".join(line_list)
--- zootopia.py
+++ zootopia.py
@@ -9,9 +9,9 @@ def init_zootopia():
     """Main function for Zootopia application."""
     sdh = SchemaDataHandler("animals_data.json", "data", AnimalMetadataSchemaRoot)  # The schema class instance.
     data = sdh.data  # Json data from file cast to the AnimalMetadataSchemaRoot class
-    search_keys = ("name", "diet", "locations", "type")  # The Keys to be searched and display in HTML.
     awg = AnimalWebGenerator("animals_template.html", "") # AnimalWebGenerator reads data from animals_template.html.
-    awg.display_key_info_in_html(data, search_keys, "animals.html") # All searched keys are written to a file animals.html
+    search_keys = ("name", "diet", "locations", "type")  # The Keys to be searched and display in HTML.
+    awg.display_key_info_in_html(data, search_keys, search_keys[0], "animals.html") # All searched keys are written to a file animals.html
 
 
 if __name__ == "__main__":
